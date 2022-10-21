ff1d30131e4ef6fed0229299f35e03cfabab2c4e
FATHANGAN <115706744+FATHANGAN@users.noreply.github.com>
2022-10-21 08:58:12 +0700

    Add files via upload

---
+++ bot.sh
@@ -0,0 +1,49 @@
+clear
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'
+yellow='\033[33;1m'
+sleep 1
+toilet -f big -F gay FATHAN
+echo $red"selamat datang"
+echo
+sleep 2
+echo $red"[1]"$green"Stabilkan Jaringan"
+echo
+echo $red"[2]"$green"Cek IP Adrees"
+echo
+echo $red"[3]"$green"Keluar Saja"
+echo
+echo $white
+read -p "Pilihan FATHAN : " bro
+
+if [ $bro = 1 ] || [ $bro = 1 ]
+then
+clear
+toilet -f big -F gay FATHAN
+echo $yellow"Stabilkan Jaringan By FATHAN"
+sleep 2
+ping -s1000 1.1.1.1
+fi
+if [ $bro = 2 ] || [ $bro = 2 ]
+then
+clear
+toilet -f big -F gay FATHAN
+echo $yellow"Cek IP Adrees By FATHAN"
+sleep 3
+ifconfig
+fi
+
+if [ $bro = 3 ] || [ $bro = 3 ]
+then
+clear
+echo $yellow
+figlet "Bye Bye"
+echo $green"FATHAN AL PATHIR"
+sleep 4
+exit
+fi
+
